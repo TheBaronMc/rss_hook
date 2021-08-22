60ec61676ef2ddc2071e1c3b8684b2c31cf7e1be
TheBaronMc <baron_mc@yahoo.com>
2021-08-22 16:03:37 +0200

    custom param file

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+// IMPORT
 const RssFeedEmitter = require('rss-feed-emitter');
+const { ArgumentParser } = require('argparse');
+const { stat, readFileSync } = require('fs');
 
 const { RSSFlux } = require('./src/rssflux');
 const { RSSHandler, RSSHandlerDefault} = require('./src/rsshandler');
+const { info } = require('console');
 
-const parameters = require('./parameters.json');
+// FUNCTION
+function checkExist(path) {
+    stat(path, (err, stats) => {
+        if (err) {
+            throw err;
+        }
+
+        if (stats.isDirectory()) {
+            throw new Error(`${path} has to be a file.`);
+        }
+    });
+}
+
+
+// PROG
+var parameters;
+
+const parser = new ArgumentParser({
+    description: 'RSS Hook'
+  });
+
+parser.add_argument('-f', '--conf_file', { help:'path to parameters.json'});
+
+args = parser.parse_args();
+
+if (args.conf_file) {
+    checkExist(args.conf_file);
+    parameters = JSON.parse(readFileSync(args.conf_file, {encoding:'utf-8'}));
+} else {
+    checkExist('./parameters.json');
+    parameters = JSON.parse(readFileSync('./parameters.json', {encoding:'utf-8'}));
+}
 
 const feeder = new RssFeedEmitter();
 
